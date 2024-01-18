64fbdb1b78013086b03967c42a78a186ff274594
Timo Hubois <info@pixelsaft.de>
2024-01-18 19:20:02 +0100

    feat(rector): extend config

--- rector.php
+++ rector.php
@@ -2,16 +2,42 @@
 
 declare(strict_types=1);
 
-use Rector\Set\ValueObject\SetList;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
+use Rector\Arguments\Rector\MethodCall\RemoveMethodCallParamRector;
+use Rector\Arguments\Rector\ClassMethod\ReplaceArgumentDefaultValueRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->autoloadPaths([
+        __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
+        __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php',
+    ]);
+
     $rectorConfig->paths([
-        __DIR__ . '/classes',
-        __DIR__ . '/components-overview-flynt.php',
+        __DIR__ . '/',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/vendor',
+        __DIR__ . '/node_modules',
     ]);
 
     $rectorConfig->sets([
-        SetList::PHP_80
+        SetList::PHP_80,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::NAMING,
+        SetList::TYPE_DECLARATION
     ]);
+
+    $rectorConfig->skip([
+        CallableThisArrayToAnonymousFunctionRector::class,
+    ]);
+
+    $rectorConfig->rule(RemoveMethodCallParamRector::class);
+    $rectorConfig->rule(ReplaceArgumentDefaultValueRector::class);
 };
